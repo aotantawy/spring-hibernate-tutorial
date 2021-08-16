e8457b7df70f47d95fb749d45ca9b71bccdedaa0
aotantawy <ahmedosama9100@gmail.com>
2021-08-16 16:49:13 +0200

    add bean life cycle code sample

---
+++ bean-life-cycle/src/main/java/spring_core/coach/Coach.java
@@ -0,0 +1,5 @@
+package spring_core.coach;
+
+public interface Coach {
+	String getDailyWorkout();
+}
---
+++ bean-life-cycle/src/main/java/spring_core/main/App.java
@@ -0,0 +1,16 @@
+package spring_core.main;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import spring_core.coach.Coach;
+
+public class App {
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+				"applicationContext.xml");
+		Coach coach = applicationContext.getBean("coach", Coach.class);
+		System.out.println(coach);
+		System.out.println(coach.getDailyWorkout());
+		applicationContext.close();
+	}
+}
